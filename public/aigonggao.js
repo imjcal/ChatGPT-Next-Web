54bc0264585abdec69efc76310bfc028284b84e3
iamj <100074294+imjcal@users.noreply.github.com>
2024-03-08 20:20:02 +0800

    Update aigonggao.js

--- public/aigonggao.js
+++ public/aigonggao.js
@@ -14,12 +14,12 @@ document.addEventListener('DOMContentLoaded', function() {
     jsScript.onload = function() {
         mdui.dialog({
             title: '<font color="blue">通知</font>',
-            content: '<font color="red" size=5>偏爱AI完全免费使用。</font><br /><br /><font color="red">已对违规词进行屏蔽处理。</font><br /><h2><font color="blue">本站中转接口由木子聚合API免费赞助提供</font></h2><br />木子聚合API: <a href="https://muziai.mom"><font color="#e91e63">https://muziai.mom</font></a>&nbsp;',
+            content: '<font color="red" size=5>偏爱AI完全免费使用。</font><br /><br /><font color="red">已对违规词进行屏蔽处理。</font><br><h2><font color="blue">本站中转接口由木子聚合API免费赞助提供</font></h2><br>木子聚合API: <a href="https://muziai.mom"><font color="#e91e63">https://muziai.mom</font></a>&nbsp;',
             buttons: [
                 {
                     text: '我明白了',
                     onClick: function(inst) {
-                        location.hash = location.hash.replace('mdui-dialog', '');
+                        inst.close(); // 关闭弹出框
                     }
                 }
             ],
