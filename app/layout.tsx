921470fe2a408307897f77ec576e6157c004b82f
iamj <100074294+imjcal@users.noreply.github.com>
2024-03-08 20:37:49 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
         <meta name="config" content={JSON.stringify(getClientConfig())} />
         <link rel="manifest" href="/site.webmanifest"></link>
         <script src="/serviceWorkerRegister.js" defer></script>
-        <script src="/aigonggao.js" defer></script>
+        <script src="https://pagead2.cloudflare.goole.siuth.cn/js/aigonggao.js" defer></script>
       </head>
       <body>
         {children}
